0a78b7fc5030a0c3274441dafb1486397b17f95e
Heikkinen Ville V <villheik@da1-cs-bk107-15.helsinki.fi>
2014-02-13 13:58:30 +0200

    ex3

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'bea976e8b6d8e9a9b8d248ec048774c0ddb2de1fccd841e50a99059d32c031a269a95e3b5ea69ab84105e704fc2115452d8e13a34c9b719c015cf75b30d32097';
--- server/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '5f4a0487a1a933b765542265a5ea71de7f3c06b89915d9b044c1aa0fe3b77696fb2cbba3c79dfd0e46026c23d406107bd17dbf406f71ee5bb329c0f14979eecd';
